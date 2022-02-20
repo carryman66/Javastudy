67e5cd97c69ae3768beb558a9f50b26d59ccd8ca
carryman66 <carryman66@7wate.com>
2022-02-20 17:40:23 +0800

    add 嵌套循环练习

---
+++ src/DoubleCycleClass.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class DoubleCycleClass {
+    public static void main(String[] args) {
+
+        /*嵌套循环练习*/
+//        for (int i = 1; i <= 7; i++) {
+//
+//            for (int j = 1; j <=2; j++) {
+//                System.out.print("1");
+//            }
+//            System.out.println();
+//
+//        }
+
+        /*统计3个班级成绩情况，每个班级有5名同学，求出各个班级的平均分和所有班级的平均分
+         成绩从键盘录入 统计3个班的及格人数*/
+
+//        Scanner scanner = new Scanner(System.in);
+//        double grade = 0;
+//        double sumGrade=0;
+//        int  passNumber=0;
+//        for (int i = 1; i <=3; i++) {
+//            for (int j = 1 ; j <=5 ; j++) {
+//                System.out.println("输入第"+j+"个"+i+"班学生成绩");
+//               double stuGrade=scanner.nextDouble();
+//               if (stuGrade>=60.0){
+//                   passNumber++;
+//               }
+//                grade += stuGrade;
+//
+//            }
+//            System.out.println(i+"班平均分为"+grade/5);
+//           sumGrade+= grade;
+//            grade=0;
+//        }
+//        System.out.println("所有班级平均分为"+sumGrade/15);
+//        System.out.println("所有班级及格人数为"+passNumber);
+
+
+    }
+}
