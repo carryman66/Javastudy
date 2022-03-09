1d3a3dbb203fea63ea6fc437d7f6b9ed5bf47ed3
carryman66 <carryman66@7wate.com>
2022-03-09 21:55:56 +0800

    add  ArrayListSource源码阅读

---
+++ src/com/carryman/collection_/test/ArrayListSource.java
@@ -0,0 +1,24 @@
+package com.carryman.collection_.test;
+
+import java.util.ArrayList;
+
+/**
+ * @author carry man
+ * @version 1.0
+ */
+public class ArrayListSource {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(i);
+        }
+
+        for (int i = 10; i < 15; i++) {
+            arrayList.add(i);
+        }
+
+
+
+
+    }
+}
