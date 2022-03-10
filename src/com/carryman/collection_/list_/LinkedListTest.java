43e76cf0d9a7ca3b4f7ca1dc0c96d6da27fa1de0
carryman66 <carryman66@7wate.com>
2022-03-10 21:49:52 +0800

    add  LinkedList链表数组学习

---
+++ src/com/carryman/collection_/list_/LinkedListTest.java
@@ -0,0 +1,38 @@
+package com.carryman.collection_.list_;
+
+import java.sql.SQLOutput;
+import java.util.LinkedList;
+
+/**
+ * @author carry man
+ * @version 1.0
+ */
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(null);
+        linkedList.add(null);
+        linkedList.add(null);
+//        linkedList.add("你好");
+//        linkedList.add("你好");
+//
+//        boolean s = linkedList.contains("你好");
+//        int index = linkedList.indexOf("你好");
+//
+//
+//        System.out.println(index);
+//        linkedList.remove("你好");
+//        linkedList.removeFirst();
+//
+//
+//
+//        linkedList.set(1,"随心所欲");
+
+
+
+        for (Object o : linkedList) {
+
+            System.out.println(o);
+        }
+    }
+}
