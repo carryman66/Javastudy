db9b86206dde32a32706dc9ec443fb64efeaf8fb
carryman66 <carryman66@7wate.com>
2022-03-11 17:39:22 +0800

    add  LinkedHashSet源码学习

---
+++ src/com/carryman/collection_/set_/LinkedHashSetTest.java
@@ -0,0 +1,19 @@
+package com.carryman.collection_.set_;
+
+import java.util.LinkedHashSet;
+
+/**
+ * @author carry man
+ * @version 1.0
+ */
+public class LinkedHashSetTest {
+    public static void main(String[] args) {
+        LinkedHashSet linkedHashSet = new LinkedHashSet();
+        linkedHashSet.add("123");
+
+        linkedHashSet.add("11123");
+        linkedHashSet.add("1123");
+
+
+    }
+}
