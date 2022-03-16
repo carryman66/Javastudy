6993c1ce6295b16dce0baf5977076128af4bedc8
carryman66 <carryman66@7wate.com>
2022-03-16 20:24:55 +0800

    add  JUnit学习

---
+++ src/com/carryman/junit/JunitTest.java
@@ -0,0 +1,21 @@
+package com.carryman.junit;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author carry man
+ * @version 1.0
+ */
+public class JunitTest {
+
+    @Test
+    public void m1(){
+        System.out.println("m1");
+    }
+
+    @Test
+    public void m2(){
+        System.out.println("m2");
+
+    }
+}
