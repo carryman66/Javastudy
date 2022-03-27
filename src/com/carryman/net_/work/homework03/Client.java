2f6b87737ab79490a433c685f4d26e2f74f9cdc1
carryman66 <carryman66@7wate.com>
2022-03-27 10:00:27 +0800

    add 网络编程作业 注释

--- src/com/carryman/net_/work/homework03/Client.java
+++ src/com/carryman/net_/work/homework03/Client.java
@@ -13,35 +13,45 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) throws IOException {
 
+        //创建套接字，指定发送位置
         Socket socket = new Socket(InetAddress.getLocalHost(), 9999);
 
+        //接收用户输入
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("输入您要下载的名称（高山流水）");
         String next = scanner.next();
 
+        //创建套接字字节输出流
         OutputStream outputStream = socket.getOutputStream();
 
+        //使用包装类，包装成字符输出流
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
 
         bufferedWriter.write(next);
+
         bufferedWriter.flush();
+        //设置输出完成标志
         socket.shutdownOutput();
 
+        //创建套接字字节输入流
         InputStream inputStream = socket.getInputStream();
 
-
+        //将输入流中的字节，转为为字节数组
         byte[] bytes1 = StreamUtils.streamToByteArray(inputStream);
-
+        //创建本地的包装缓存输出流，下载文件，写入磁盘
         BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("d:\\新建文件夹\\高山流水.jpg"));
-
+        //写入磁盘
         bufferedOutputStream.write(bytes1);
 
 
-
+        //关闭下载的流
         bufferedOutputStream.close();
+        //关闭从服务器输入的流
         inputStream.close();
+        //关闭输出流
         bufferedWriter.close();
+        //关闭套接字
         socket.close();
 
 
